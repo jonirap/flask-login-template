fa32e8c34b95623a7afd014211ff37baa6ec6590
Noa Luk-Zilberman <noaluk@gmail.com>
2017-08-24 19:55:16 +0300

    updated state'

--- app/events/IncidentState.py
+++ app/events/IncidentState.py
@@ -4,4 +4,4 @@ from flask.views import MethodView
 class IncidentStateView(MethodView):
     def get(self):
         incident_id = request.data['incidentID']
-        # todo: return helpers count, message etc
+        # todo: return helpers count, message...
--- config.py
+++ config.py
@@ -52,6 +52,6 @@ ADMINS = ['keyz182@gmail.com']
 
 DEBUG = True
 
-APNS_CERTIFICATE = r"C:\Users\Noaluk\Projects\flask-login-template\ios_development.cer"
+APNS_CERTIFICATE = r"C:\Users\Noaluk\Projects\flask-login-template\pushcert.pem"
 
 
