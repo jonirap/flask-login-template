a2528d031bfbe7aca5d8a6b428253050917dc53e
Noa Luk-Zilberman <noaluk@gmail.com>
2017-08-24 19:55:16 +0300

    updated state'

--- config.py
+++ config.py
@@ -48,6 +48,6 @@ ADMINS = ['keyz182@gmail.com']
 
 DEBUG = True
 
-APNS_CERTIFICATE = r"C:\Users\Noaluk\Projects\flask-login-template\ios_development.cer"
+APNS_CERTIFICATE = r"C:\Users\Noaluk\Projects\flask-login-template\pushcert.pem"
 
 
