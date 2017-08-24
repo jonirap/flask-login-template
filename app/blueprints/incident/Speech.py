cbffa0c08395e74cc18b1a30afd0f81c45b1bfdc
Tal Friedman <talfridmen@walla.com>
2017-08-24 17:45:06 +0300

    Now it is working!! oot oot

--- app/blueprints/incident/Speech.py
+++ app/blueprints/incident/Speech.py
@@ -1,6 +1,6 @@
 import os
 from consts import SPEECH_FOLDER, TEXT_FOLDER
-from flask import request
+from flask import request, jsonify
 from flask.views import MethodView
 
 from SpeechToText import convert_audio_file
@@ -8,13 +8,12 @@ from SpeechToText import convert_audio_file
 
 class IncidentSpeechView(MethodView):
     def post(self):
-        print 'received!'
-        f = request.files['file']
+        f = request.files.values()[0]
         filename = f.filename
         local_speech_file_path = os.path.join(SPEECH_FOLDER, filename)
         local_text_file_path = os.path.join(TEXT_FOLDER, filename)
         f.save(local_speech_file_path)
-
         data_text = convert_audio_file(local_speech_file_path)
         with open(local_text_file_path, 'wb') as text_file:
             text_file.write(data_text.encode('utf-8'))
+        return jsonify(ok=True)
--- consts.py
+++ consts.py
@@ -1,5 +1,5 @@
 import os
 
 
-SPEECH_FOLDER = os.path.join(__file__, 'speech')
-TEXT_FOLDER = os.path.join(__file__, 'text')
+SPEECH_FOLDER = os.path.join(__file__, '..', 'speech')
+TEXT_FOLDER = os.path.join(__file__, '..', 'text')
