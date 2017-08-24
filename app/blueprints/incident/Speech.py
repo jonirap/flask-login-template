ed0f647d2bfd3664cdd871fbfbb9a7c9843a9758
Tal Friedman <talfridmen@walla.com>
2017-08-24 22:34:15 +0300

    write to db and extract funcs

--- app/blueprints/incident/Speech.py
+++ app/blueprints/incident/Speech.py
@@ -3,9 +3,10 @@ import os
 import pydub
 
 from Categories import Category
-from consts import SPEECH_FOLDER, TEXT_FOLDER
+from consts import SPEECH_FOLDER
 from flask import request, jsonify
 from flask.views import MethodView
+from app.auth.models import Incident
 
 from SpeechToText import convert_audio_file
 
@@ -15,20 +16,24 @@ class IncidentSpeechView(MethodView):
         f = request.files.values()[0]
         filename = f.filename
         local_speech_file_path = os.path.join(SPEECH_FOLDER, filename)
-        local_text_file_path = os.path.join(TEXT_FOLDER, filename)
         f.save(local_speech_file_path)
-        # import ipdb; ipdb.set_trace()
         f.close()
         if not filename.endswith('.wav'):
-            wav_file = pydub.AudioSegment.from_file(local_speech_file_path).export(
-                local_speech_file_path.rsplit('.', 1)[0] + '.wav', format='wav')
-            os.remove(local_speech_file_path)
-            local_speech_file_path = wav_file.name
-            wav_file.close()
+            local_speech_file_path = convert_to_wav(local_speech_file_path)
         data_text = convert_audio_file(local_speech_file_path).encode('utf-8')
-        with open(local_text_file_path, 'wb') as text_file:
-            text_file.write(data_text)
 
         category = Category.get(data_text)
+        incident = Incident(lat=10.5, long=12.2, audio_file_path=local_speech_file_path,
+                            description=data_text, in_need_id=1, helpers=[], status="", category=category)
 
-        return jsonify(category=category)
+
+        return jsonify(incident_id=incident.id)
+
+
+def convert_to_wav(not_wav_file_path):
+    wav_file = pydub.AudioSegment.from_file(not_wav_file_path).export(
+        not_wav_file_path.rsplit('.', 1)[0] + '.wav', format='wav')
+    os.remove(not_wav_file_path)
+    local_speech_file_path = wav_file.name
+    wav_file.close()
+    return local_speech_file_path
