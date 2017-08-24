f16c0d434cefcbacd1f6bf8a850cf3756f987dd1
nir geller <geller.nir@gmail.com>
2017-08-24 19:07:06 +0300

    test new db

--- dbutils/db_init.py
+++ dbutils/db_init.py
@@ -6,8 +6,10 @@ def InitDB():
     print "Pre-populating DB"
 
     u1 = User(allergies='Cats', id_number='209499359', blood_type='A+', username='gNir', uuid='12345678901234567890')
-
-    db.session.add(u1)
+    u2 = User(allergies='Cats', id_number='204499359', blood_type='B+', username='joni', uuid='12345678901234567890')
+    i1 = Incident(lat=10.5, long=12.2, audio_file_path="tmp/a.avi",
+                  description="Joni gay", in_need_id=u1, helpers=[u2])
+    db.session.add_all([u1, u2, i1])
     db.session.commit()
 
     users = User.query.all()
@@ -18,4 +20,4 @@ def InitDB():
 
 if __name__ == '__main__':
     for user in User.query.all():
-        print user.blood_type
+        print user
