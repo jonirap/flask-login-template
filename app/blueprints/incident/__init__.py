28eb90983db00d6d96f99d97bd34873b6ae395e0
Tal Friedman <talfridmen@walla.com>
2017-08-24 22:05:54 +0300

    shit now works!

--- app/blueprints/incident/__init__.py
+++ app/blueprints/incident/__init__.py
@@ -1,10 +1,8 @@
 from flask import Blueprint
 from Speech import IncidentSpeechView
-from Text import IncidentTextView
 
 incident = Blueprint('incident', __name__)
 incident.add_url_rule('/speech', view_func=IncidentSpeechView.as_view('speech'))
-incident.add_url_rule('/text', view_func=IncidentTextView.as_view('text'))
 
 
 class View:
