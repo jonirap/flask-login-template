f7dba7451fe2a3230afe47428d981eb6cabc25bc
Noa Luk-Zilberman <noaluk@gmail.com>
2017-08-24 19:07:39 +0300

    changed names

--- app/events/__init__.py
+++ app/events/__init__.py
@@ -5,13 +5,13 @@ from flask import Blueprint
 
 from app.events.Notifications import NotificationsView
 
-notifications = Blueprint('event', __name__, template_folder='templates/events')
-notifications.add_url_rule('/help', view_func=NotificationsView.as_view('notifications'))
-notifications.add_url_rule('/i_want_to_help', view_func=VolunteerView.as_view('volunteer'))
-notifications.add_url_rule('/incident_state', view_func=IncidentStateView.as_view('incident_state'))
-notifications.add_url_rule('/finished', view_func=IncidentEndView.as_view('finished'))
+events = Blueprint('event', __name__, template_folder='templates/events')
+events.add_url_rule('/help', view_func=NotificationsView.as_view('notifications'))
+events.add_url_rule('/i_want_to_help', view_func=VolunteerView.as_view('volunteer'))
+events.add_url_rule('/incident_state', view_func=IncidentStateView.as_view('incident_state'))
+events.add_url_rule('/finished', view_func=IncidentEndView.as_view('finished'))
 
 
 class View:
     def get_blueprint(self):
-        return notifications
+        return events
